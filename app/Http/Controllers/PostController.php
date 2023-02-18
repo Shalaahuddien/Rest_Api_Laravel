6f88d52436ec84858bfd67ed6a570c07bcea3277
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-18 22:19:53 +0700

    lanjut

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
 
     public function show($id)
     {
-        $post = Post::findOrFail($id);
+        $post = Post::with('writer:id,username')->findOrFail($id);
         return new PostDetailResource($post);
     }
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -14,4 +15,11 @@ class Post extends Model
         'news_content',
         'author'
     ];
+
+
+    public function writer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author', 'id');
+    }
+
 }
