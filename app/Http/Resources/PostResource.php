85084d3dcf73d90fc75ab45f8f43ecb02851144c
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-18 21:23:21 +0700

    lanjut

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class PostResource extends JsonResource
 {
@@ -14,6 +15,11 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'news_content' => $this->news_content,
+            'created_at' => date_format($this->created_at,"Y/m/d H:i:s")
+        ];
     }
 }
