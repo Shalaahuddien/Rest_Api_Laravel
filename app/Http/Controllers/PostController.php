520b25d01defbb991b0215656b69c889034e058c
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-18 18:09:37 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json($posts);
+    }
+
+}
